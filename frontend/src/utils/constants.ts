29262fae7dacd00a0594b3ecc7809bf100dfea25
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2026-05-07 16:01:46 -0500

    Actualizar constantes para usar variables de entorno en lugar de rutas relativas para las URLs de API

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,6 +1,6 @@
-// Usar rutas relativas para aprovechar el proxy de Vite
-const URL_API_LOGIN = '/api/v2'
-const URL_API = '/api_data'
+// Usar variables de entorno para las URLs de API
+const URL_API_LOGIN = import.meta.env.VITE_API_URL_LOGIN || '/api_login'
+const URL_API = import.meta.env.VITE_API_URL || '/api_data'
 const COMPANY = import.meta.env.VITE_APP_ENV
 
 export { URL_API_LOGIN, URL_API, COMPANY }
