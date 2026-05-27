e3b666d4d87c9284884209d7d4d57e284e0a2d53
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2026-05-27 08:17:31 -0500

    Actualizar constantes de API para usar variables de entorno y ajustar la configuración de Nginx para el proxy de API

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,6 +1,6 @@
 // Usar rutas relativas para aprovechar el proxy de Vite
-const URL_API_LOGIN = '/api_login'
-const URL_API = '/api_data'
+const URL_API_LOGIN = import.meta.env.VITE_API_URL_LOGIN
+const URL_API = import.meta.env.VITE_API_URL
 const COMPANY = import.meta.env.VITE_APP_ENV
 
 export { URL_API_LOGIN, URL_API, COMPANY }
